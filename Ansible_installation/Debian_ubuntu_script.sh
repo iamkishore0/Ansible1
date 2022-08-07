2adc8a997ef7950443d9b0084a0ea58e45a091fe
Devops_life <98376417+devopslife999@users.noreply.github.com>
2022-08-07 16:01:45 +0530

    Update Debian_ubuntu_script.sh

--- Ansible_installation/Debian_ubuntu_script.sh
+++ Ansible_installation/Debian_ubuntu_script.sh
@@ -1,7 +1,7 @@
-sudo apt-add-repository ppa:ansible/ansible &&
-sudo apt update &&
-sudo apt install ansible &&
-sudo apt-get update &&
-sudo apt-get install python3.6 &&
-sudo apt install python3-pip &&
-pip install boto
+sudo apt-add-repository ppa:ansible/ansible -y &&
+sudo apt update -y &&
+sudo apt install ansible -y &&
+sudo apt-get update -y &&
+sudo apt-get install python3.6 -y &&
+sudo apt install python3-pip -y &&
+pip install boto -y
